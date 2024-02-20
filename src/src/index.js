1c9cf49ed12bfa2ceaeebcf99b00e67f98922730
Олександр Канюка <137859612+crusaider423@users.noreply.github.com>
2024-02-20 17:21:35 +0200

    u

--- src/src/index.js
+++ src/src/index.js
@@ -8,6 +8,7 @@ import store from './redux/store';
 import { persistor } from './redux/store';
 import './index.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
